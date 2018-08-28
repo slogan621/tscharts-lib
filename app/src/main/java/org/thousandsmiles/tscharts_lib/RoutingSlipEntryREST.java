b935957a4c00ee64ce896fe8827830ea0dcb6daf
Syd Logan <slogan621@gmail.com>
2018-08-27 20:58:30 -0700

    clean up status code handling

--- app/src/main/java/org/thousandsmiles/tscharts_lib/RoutingSlipEntryREST.java
+++ app/src/main/java/org/thousandsmiles/tscharts_lib/RoutingSlipEntryREST.java
@@ -65,17 +65,18 @@ public class RoutingSlipEntryREST extends RESTful {
         public void onErrorResponse(VolleyError error) {
 
             synchronized (m_lock) {
+                int code;
                 if (error.networkResponse == null) {
                     if (error.getCause() instanceof java.net.ConnectException || error.getCause() instanceof  java.net.UnknownHostException) {
-                        setStatus(101);
-                        onFail(101, "");
+                        code = 101;
                     } else {
-                        setStatus(-1);
-                        onFail(101, "");
+                        code = -1;
                     }
                 } else {
-                   setStatus(error.networkResponse.statusCode);
+                    code = error.networkResponse.statusCode;
                 }
+                setStatus(code);
+                onFail(code, "");
                 m_lock.notify();
             }
         }
