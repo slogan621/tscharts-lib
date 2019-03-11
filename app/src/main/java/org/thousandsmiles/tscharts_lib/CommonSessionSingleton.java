ebb0d955797500c913d5c2f3c6ce2e4c056e0155
Syd Logan <slogan621@gmail.com>
2019-03-10 19:20:08 -0700

    fix date format string again :-)

--- app/src/main/java/org/thousandsmiles/tscharts_lib/CommonSessionSingleton.java
+++ app/src/main/java/org/thousandsmiles/tscharts_lib/CommonSessionSingleton.java
@@ -152,7 +152,7 @@ public class CommonSessionSingleton {
                     for (int i = 0; i < a.length(); i++) {
                         Date date, today;
 
-                        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy HH:MM:SS");
+                        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
                         sdf.setLenient(false);
                         try {
                             date = sdf.parse(a.getJSONObject(i).getString("time"), new ParsePosition(0));
