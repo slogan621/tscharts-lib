1489b25d4de73a5dc29491d7ad4536583d8f1f21
Syd Logan <slogan621@gmail.com>
2021-04-11 10:03:34 -0700

    invoke all REST listeners on UI thread, add locks around accesses to listener registration list

--- app/src/main/java/org/thousandsmiles/tscharts_lib/RESTful.java
+++ app/src/main/java/org/thousandsmiles/tscharts_lib/RESTful.java
@@ -1,6 +1,6 @@
 /*
- * (C) Copyright Syd Logan 2017-2018
- * (C) Copyright Thousand Smiles Foundation 2017-2018
+ * (C) Copyright Syd Logan 2017-2021
+ * (C) Copyright Thousand Smiles Foundation 2017-2021
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,8 @@ package org.thousandsmiles.tscharts_lib;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Looper;
 import android.preference.PreferenceManager;
 
 import org.json.JSONArray;
@@ -26,39 +28,67 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
+/* assumption - application has no thread local storage, and it is safe (if not required) to invoke all callbacks on UI thread */
+
 public abstract class RESTful {
     private Context m_context;
     private int m_status;
     private int m_timeout = 5000;  // Milliseconds
     private int m_retries = 3;
     private ArrayList<RESTCompletionListener> m_listener = new ArrayList<RESTCompletionListener>();
+    private Object lock = new Object();
 
     public void addListener(RESTCompletionListener o) {
-        m_listener.add(o);
+        synchronized(lock) {
+            m_listener.add(o);
+        }
     }
 
     public void removeListener(RESTCompletionListener o) {
-        m_listener.remove(o);
-    }
-
-    protected void onSuccess(int code, String message, JSONArray a)
-    {
-        for (RESTCompletionListener x : m_listener) {
-            x.onSuccess(code, message, a);
+        synchronized(lock) {
+            m_listener.remove(o);
         }
     }
 
-    protected void onSuccess(int code, String message, JSONObject o)
+    protected void onSuccess(final int code, final String message, final JSONArray a)
     {
-        for (RESTCompletionListener x : m_listener) {
-            x.onSuccess(code, message, o);
+        synchronized(lock) {
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                @Override
+                public void run() {
+                    for (RESTCompletionListener x : m_listener) {
+                        x.onSuccess(code, message, a);
+                    }
+                }
+            });
         }
     }
 
-    protected void onSuccess(int code, String message)
+    protected void onSuccess(final int code, final String message, final JSONObject o)
     {
-        for (RESTCompletionListener x : m_listener) {
-            x.onSuccess(code, message);
+        synchronized(lock) {
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                @Override
+                public void run() {
+                    for (RESTCompletionListener x : m_listener) {
+                        x.onSuccess(code, message, o);
+                    }
+                }
+            });
+        }
+    }
+
+    protected void onSuccess(final int code, final String message)
+    {
+        synchronized(lock) {
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                @Override
+                public void run() {
+                    for (RESTCompletionListener x : m_listener) {
+                        x.onSuccess(code, message);
+                    }
+                }
+            });
         }
     }
 
@@ -72,10 +102,17 @@ public abstract class RESTful {
         return m_retries;
     }
 
-    protected void onFail(int code, String message)
+    protected void onFail(final int code, final String message)
     {
-        for (RESTCompletionListener x : m_listener) {
-            x.onFail(code, message);
+        synchronized(lock) {
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                @Override
+                public void run() {
+                    for (RESTCompletionListener x : m_listener) {
+                        x.onFail(code, message);
+                    }
+                }
+            });
         }
     }
 
