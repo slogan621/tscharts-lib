7052cfa1f4c7e41b14b862bcad4ca9677d07dbf1
Syd Logan <slogan621@gmail.com>
2022-12-28 13:05:59 -0800

    integrate zebra print codes

--- app/src/main/java/org/thousandsmiles/tscharts_lib/ZebraWristbandPrinter.kt
+++ app/src/main/java/org/thousandsmiles/tscharts_lib/ZebraWristbandPrinter.kt
@@ -20,10 +20,7 @@ package org.thousandsmiles.tscharts_lib
 import com.zebra.sdk.comm.Connection
 import com.zebra.sdk.comm.ConnectionException
 import com.zebra.sdk.comm.TcpConnection
-import com.zebra.sdk.printer.SGD
-import com.zebra.sdk.printer.ZebraPrinter
-import com.zebra.sdk.printer.ZebraPrinterFactory
-import com.zebra.sdk.printer.ZebraPrinterLanguageUnknownException
+import com.zebra.sdk.printer.*
 
 class ZebraWristbandPrinter(ipAddr: String?, port: Int?) : WristbandPrinter(ipAddr, port) {
 
@@ -44,9 +41,56 @@ class ZebraWristbandPrinter(ipAddr: String?, port: Int?) : WristbandPrinter(ipAd
             notifyOnError(job, m_printerStatus, "Unable to connect to printer")
             return false
         }
+        var configLabel: ByteArray? = null
 
-        // ....
+        try {
+            val printerLanguage: PrinterLanguage = m_printer!!.getPrinterControlLanguage()
+            SGD.SET("device.languages", "zpl", m_connection)
+            if (printerLanguage == PrinterLanguage.ZPL) {
 
+                /*
+                     String position of the text depending on wristband size. QR position will be screwed up.
+                     String position = "400";      // For 6" wristband
+                     String position = "400";      // for 7" wristband
+                 */
+
+                val position = "2000" // for 11" wristband
+                /*
+                   Format the QR code. this does position, QR Code magnification, and inserts the ID,FATHER_LAST,FIRST_NAME
+                    All comma separated
+                 */
+                val qrcode = "^FO80,1800^BQN,2,6^FDQA," + patient.getId() + "," + patient.getFatherLast() + "," + patient.getFirst() + "^FS"
+                /*
+                    Format the text line data:
+                        Line 1: ID CURP
+                        Line 2: Father's Last, Mother's last
+                        Line 3: First Name, Middle Name
+                        Line 4: DOB, Male/Female
+                 */
+                val line1Text = "ID: " + patient.getId() + "  CURP: " + patient.getCURP()
+                val line2Text: String = patient.getFatherLast() + ", " + patient.getMotherLast()
+                val line3Text: String = patient.getFirst() + " " + patient.getMiddle()
+                val line4Text: String = patient.getDob() + " " + patient.getGender()
+                /*
+                    Format the four line with position and font info
+                */
+                val line1 = "^XA^FO1,10^FS^FT225,$position^A0R,40,40^FD$line1Text ^FS"
+                val line2 = "^FO1,10^FS^FT168,$position^A0R,40,40^FD$line2Text ^FS"
+                val line3 = "^FO1,10^FS^FT111,$position^A0R,40,40^FD$line3Text^FS"
+                val line4 = "^FO1,10^FS^FT54,$position^A0R,40,40^FD$line4Text^FS$qrcode^XZ"
+                /*
+                    Concatenate the four line into a single string and
+                    return the complete image to the calling routine to print
+                */
+                val fullLine = line1 + line2 + line3 + line4
+                configLabel = fullLine.toByteArray()
+            } else if (printerLanguage == PrinterLanguage.CPCL) {
+                return false
+            }
+        } catch (e: ConnectionException) {
+        }
+        m_connection?.write(configLabel)
+        
         disconnect(job);
         notifyOnSuccess(job, m_printerStatus)
         return true
